5bd0541de29d3d440e6b61536382c7c24e1f00d3
Sinkler521 <131878372+Sinkler521@users.noreply.github.com>
2024-03-03 14:58:31 +0200

    added translations skeleton

---
+++ src/translations/languages/messages.js
@@ -0,0 +1,8 @@
+import {pl} from './pl'
+import {ru} from './ru'
+
+export const messages = {
+    POLISH: pl,
+    RUSSIAN: ru,
+}
+
---
+++ src/translations/translationsConfig.js
@@ -0,0 +1,11 @@
+import {LOCALES} from "./locales";
+
+export const DEFAULTLANGUAGE = "ru";
+export let CURRENTLANGUAGE = DEFAULTLANGUAGE
+export const LANGUAGES = LOCALES
+
+export default {
+  DEFAULTLANGUAGE,
+  CURRENTLANGUAGE,
+  LANGUAGES
+};
