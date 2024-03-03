f876ee1650a9a0079afa3deb899fb914dc29d900
Sinkler521 <131878372+Sinkler521@users.noreply.github.com>
2024-03-03 14:59:31 +0200

    changed App.js and index.js to provide possibility of translations and routes usage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import {useEffect, useState} from "react";
+import React from 'react';
 import './App.css';
+import {LOCALES} from "./translations/locales";
+import translationsConfig from "./translations/translationsConfig";
+import {messages} from "./translations/languages/messages";
+import {IntlProvider} from "react-intl";
+import {Routes, Route} from "react-router";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App(){
+    const [host, setHost] = useState(process.env.host)
+    const [currentMessages, setCurrentMessages] = useState('')
+
+    useEffect(() => {
+        const languageKey = Object.keys(LOCALES).find(key => LOCALES[key] === translationsConfig.CURRENTLANGUAGE);
+        setCurrentMessages(messages[languageKey])
+    }, [translationsConfig.CURRENTLANGUAGE]);
+
+    const changeLanguage = (e) => {
+        const newLanguage = translationsConfig.CURRENTLANGUAGE === 'en' ? 'ru' : 'en';
+        translationsConfig.CURRENTLANGUAGE = newLanguage;
+        setCurrentMessages(messages[newLanguage]);
+
+        e.target.innerText = translationsConfig.CURRENTLANGUAGE
+
+        localStorage.setItem('CURRENTLANGUAGE', newLanguage);
+    }
+
+    return (
+        <>
+            <div className="App">
+                <IntlProvider locale={translationsConfig.CURRENTLANGUAGE} defaultLocale={translationsConfig.CURRENTLANGUAGE} messages={currentMessages}>
+                    <Routes>
+                        {/*<Route path="/" element={<Mainpage changeLanguage={changeLanguage} host={host}/>} />*/}
+                        {/*<Route path="/login" element={<Login host={host}/>}/>*/}
+                    </Routes>
+                </IntlProvider>
+            </div>
+        </>
+    )
+
 }
 
-export default App;
