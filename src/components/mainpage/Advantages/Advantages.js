51beb7be01aaad7a1eacb160ec3137c5c1acb9c9
Sinkler521 <131878372+Sinkler521@users.noreply.github.com>
2024-03-11 04:10:25 +0200

    added new library for animations and replaced classic css with this lib usage

--- src/components/mainpage/Advantages/Advantages.js
+++ src/components/mainpage/Advantages/Advantages.js
@@ -2,76 +2,88 @@ import React, {useState, useEffect, useRef} from "react";
 import './Advantages.css'
 import reviewImg1 from '../../../assets/images/advantages/review-1.jpg';
 import reviewImg2 from '../../../assets/images/advantages/review-2.jpg';
+import {easeOut, motion, useAnimation} from 'framer-motion';
 
 export const Advantages = (props) => {
     const [isFirstReviewVisible, setIsFirstReviewVisible] = useState(true);
     const animatedElements = useRef([])
+    const controls = useAnimation();
     const reviews = useRef([]);
 
     useEffect(() => {
         const interval = setInterval(() => {
             setIsFirstReviewVisible(prevState => !prevState);
         }, 10000);
+        return(
+            clearInterval(interval)
+        )
+    }, [])
 
+    useEffect(() => {
         const handleScroll = () => {
-            if (window.scrollY > window.innerHeight * 2){
-                let delay = 0;
-                animatedElements.current.forEach((element) => {
-                    setTimeout(() => {
-                        element.classList.add('scroll-active');
-                    }, delay);
-                    delay += 400;
-                });
-            }
+            animatedElements.current.forEach((element, index) => {
+                const elementTop = element.getBoundingClientRect().top;
+                const viewportHeight = window.innerHeight;
+
+                if (elementTop < viewportHeight) {
+                    controls.start({ x: 0, opacity: 1, transition: { duration: 0.45, type: "Tween"} });
+                }
+            });
         };
 
         window.addEventListener('scroll', handleScroll);
 
         return () => {
-            clearInterval(interval);
             window.removeEventListener('scroll', handleScroll);
         };
-    }, []);
+    }, [controls]);
 
     return (
         <div className="advantages-container">
             <div className="advantages-content">
-                <div className="advantages-info-block scroll-right" ref={(element) => animatedElements.current[0] = element}>
+                <motion.div className="advantages-info-block" ref={(element) => animatedElements.current[0] = element} initial={{ x: '-100%', opacity: 0 }} animate={controls}>
                     <h2>Что можно делать с нами?</h2>
                     <p>Мы хотим предложить Вам самые лучшие условия</p>
-                </div>
+                </motion.div>
                 <div className="advantages-advantages-block">
-                    <div className="scroll-left" ref={(element) => animatedElements.current[1] = element}>
+                    <motion.div ref={(element) => animatedElements.current[1] = element}
+                                initial={{x: '100%', opacity: 0}} animate={controls}>
                         <div className="advantages-advantages-content">
                             <span className="advantages-icon"><i className="fa-solid fa-book"></i></span>
                             <h2>Новые знания</h2>
                             <p>Расширяйте границы познания используя возможности EducateMe.</p>
                         </div>
-                    </div>
-                    <div className="scroll-left" ref={(element) => animatedElements.current[2] = element}>
+                    </motion.div>
+                    <motion.div ref={(element) => animatedElements.current[2] = element}
+                                initial={{x: '100%', opacity: 0}} animate={controls}>
                         <div className="advantages-advantages-content">
                             <span className="advantages-icon"><i className="fa-regular fa-clock"></i></span>
                             <h2>Идеальный график</h2>
-                            <p>Выбирайте время для учебы самостоятельно. Пусть обучение будет для Вас максимально удобным.</p>
+                            <p>Выбирайте время для учебы самостоятельно. Пусть обучение будет для Вас максимально
+                                удобным.</p>
                         </div>
-                    </div>
-                    <div className="scroll-left" ref={(element) => animatedElements.current[3] = element}>
+                    </motion.div>
+                    <motion.div ref={(element) => animatedElements.current[3] = element}
+                                initial={{x: '100%', opacity: 0}} animate={controls}>
                         <div className="advantages-advantages-content">
                             <span className="advantages-icon"><i className="fa-solid fa-atom"></i></span>
                             <h2>Большой выбор</h2>
-                            <p>На платформе представлено большое количество развивающих материалов. Подберите то, что идеально Вам подойдет.</p>
+                            <p>На платформе представлено большое количество развивающих материалов. Подберите то, что
+                                идеально Вам подойдет.</p>
                         </div>
-                    </div>
+                    </motion.div>
                 </div>
                 <div className="advantages-reviews">
                     <div className={`advantages-review ${isFirstReviewVisible ? '' : 'review-hide'}`}
                          ref={el => reviews.current[0] = el}>
-                        <h2>"Давно решил для себя что саморазвитие важнее всего. EducateMe мне в этом хороший помощник."</h2>
+                        <h2>"Давно решил для себя что саморазвитие важнее всего. EducateMe мне в этом хороший
+                            помощник."</h2>
                         <img src={reviewImg1} alt="no image" className="advantages-review-img" loading="lazy"/>
                         <span className="advantages-review-name"><strong>Ronald McGivern</strong></span>
                         <span className="advantages-review-position">developer</span>
                     </div>
-                    <div className={`advantages-review ${isFirstReviewVisible ? 'review-hide' : ''}`} ref={el => reviews.current[1] = el}>
+                    <div className={`advantages-review ${isFirstReviewVisible ? 'review-hide' : ''}`}
+                         ref={el => reviews.current[1] = el}>
                         <h2>"Отлично составленные, понятные уроки. Теперь я знаю и умею намного больше."</h2>
                         <img src={reviewImg2} alt="no image" className="advantages-review-img" loading="lazy"/>
                         <span className="advantages-review-name"><strong>Claire Williams</strong></span>
@@ -82,3 +94,4 @@ export const Advantages = (props) => {
         </div>
     );
 };
+
--- src/components/mainpage/Statistics/Statistics.js
+++ src/components/mainpage/Statistics/Statistics.js
@@ -2,21 +2,22 @@ import React, {useState, useEffect, useRef} from "react";
 import './Statistics.css';
 import student from '../../../assets/images/statistics/student.png';
 import teacher from '../../../assets/images/statistics/teacher.png';
+import { motion, useAnimation } from 'framer-motion';
 
 export const Statistics = (props) => {
-    const animatedElements = useRef([])
+    const animatedElements = useRef([]);
+    const controls = useAnimation();
 
     useEffect(() => {
         const handleScroll = () => {
-            if (window.scrollY > window.innerHeight / 2) {
-                let delay = 0;
-                animatedElements.current.forEach((element) => {
-                    setTimeout(() => {
-                        element.classList.add('scroll-active');
-                    }, delay);
-                    delay += 300;
-                });
-            }
+            animatedElements.current.forEach((element, index) => {
+                const elementTop = element.getBoundingClientRect().top;
+                const viewportHeight = window.innerHeight;
+
+                if (elementTop < viewportHeight) {
+                    controls.start({ x: 0, opacity: 1, transition: { duration: 0.5, type: "Tween" } });
+                }
+            });
         };
 
         window.addEventListener('scroll', handleScroll);
@@ -24,36 +25,33 @@ export const Statistics = (props) => {
         return () => {
             window.removeEventListener('scroll', handleScroll);
         };
-    }, []);
+    }, [controls]);
 
-    // TODO пользователей, всего курсов, успешно пройдено
     return (
-        <>
-            <div className="statistics-container">
-                <div className="statistics-content">
-                    <div className="statistics-info-block scroll-right" ref={(element) => animatedElements.current[0] = element}>
-                        <h2>Откройте Двери Знаний. Никогда не поздно начать учиться!</h2>
-                        <p>Возможности обучения не имеют пределов, и с каждым новым шагом вы приближаетесь к своим целям
-                            и мечтам. Начните сегодня - ведь обучение никогда не заканчивается.</p>
-                    </div>
-                    <div className="statistics-data-block scroll-left" ref={(element) => animatedElements.current[1] = element}>
-                        <div><h3>153</h3><h3>Пользователей</h3></div>
-                        <div><h3>456</h3><h3>Курсов на сайте</h3></div>
-                        <div><h3>288</h3><h3>Успешно пройдено</h3></div>
-                    </div>
-                    <div className="statistics-students-block">
-                        <h3>Вы студент?</h3>
-                        <p>С нашим разнообразным выбором курсов и обучающих материалов вы сможете изучать что угодно - от академических предметов до навыков саморазвития. Возьмите обучение в свои руки с EducateMe и погрузитесь в мир бесконечных возможностей!</p>
-                        <img src={student} alt="no image" id="statistics-student" loading="lazy"/>
-                    </div>
-                    <div className="statistics-teachers-block">
-                        <h3>Вы учитель?</h3>
-                        <p>Присоединяйтесь к нашему сообществу образования на EducateMe и делитесь своими знаниями с
-                            миром.</p>
-                        <img src={teacher} alt="no image" id="statistics-teacher" loading="lazy"/>
-                    </div>
+        <div className="statistics-container">
+            <div className="statistics-content">
+                <motion.div className="statistics-info-block" ref={(element) => animatedElements.current[0] = element} initial={{ x: '100%', opacity: 0 }} animate={controls}>
+                    <h2>Откройте Двери Знаний. Никогда не поздно начать учиться!</h2>
+                    <p>Возможности обучения не имеют пределов, и с каждым новым шагом вы приближаетесь к своим целям
+                        и мечтам. Начните сегодня - ведь обучение никогда не заканчивается.</p>
+                </motion.div>
+                <motion.div className="statistics-data-block" ref={(element) => animatedElements.current[1] = element} initial={{ x: '-100%', opacity: 0 }} animate={controls}>
+                    <div><h3>153</h3><h3>Пользователей</h3></div>
+                    <div><h3>456</h3><h3>Курсов на сайте</h3></div>
+                    <div><h3>288</h3><h3>Успешно пройдено</h3></div>
+                </motion.div>
+                <div className="statistics-students-block">
+                    <h3>Вы студент?</h3>
+                    <p>С нашим разнообразным выбором курсов и обучающих материалов вы сможете изучать что угодно - от академических предметов до навыков саморазвития. Возьмите обучение в свои руки с EducateMe и погрузитесь в мир бесконечных возможностей!</p>
+                    <img src={student} alt="no image" id="statistics-student" loading="lazy"/>
+                </div>
+                <div className="statistics-teachers-block">
+                    <h3>Вы учитель?</h3>
+                    <p>Присоединяйтесь к нашему сообществу образования на EducateMe и делитесь своими знаниями с
+                        миром.</p>
+                    <img src={teacher} alt="no image" id="statistics-teacher" loading="lazy"/>
                 </div>
             </div>
-        </>
-    )
+        </div>
+    );
 }
