d7207b107b305f5fb879b24ad9b4ba843cf1363e
Sinkler521 <131878372+Sinkler521@users.noreply.github.com>
2024-03-18 02:34:05 +0200

    added form validation and request (doesn't work properly yet)

---
+++ src/components/contact/ContactForm/ContactForm.js
@@ -0,0 +1,106 @@
+import React, {useState, useEffect, useRef} from "react";
+import ReCAPTCHA from "react-google-recaptcha";
+import {regexPatterns} from "../../../helpers/regex";
+import {NotificationComponent} from "../../NotificationComponent/NotificationComponent";
+import './ContactForm.css';
+import {toast} from "react-toastify";
+import axios from "axios";
+import cookie from "react-cookies";
+
+export const ContactForm = (props) => {
+    const [captchaValue, setCaptchaValue] = useState(null);
+
+    const emailInput = useRef(null);
+    const messageInput = useRef(null);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        let email,
+            message = messageInput.current.value;
+
+        if(emailInput.current['data-valid']){
+            email = emailInput.current.value;
+        } else{
+            toast.warning('Неверный емейл')
+            return;
+        }
+
+        if(!captchaValue){
+            toast.warning('Пройдите капчу')
+            return;
+        }
+        try {
+
+            const response = await axios.post(`${props.host.api}/contactus`,
+                {
+                email: email,
+                message: message,
+            }, {
+            headers: {
+              "Content-Type": "application/json",
+              "X-CSRFToken": cookie.load("csrftoken"),
+            }})
+
+            if(response.data.status === 'success'){
+                toast.success('Сообщение успешно отправлено')
+            } else if(response.data.status === 'failed'){
+                toast.warning('Сообщение не было доставлено')
+            }
+        } catch (e) {
+            toast.warning('Что-то пошло не так')
+        }
+    }
+
+    const handleCaptchaChange = (value) => {
+        setCaptchaValue(value);
+    }
+
+    const handleInputChange = (e) => {
+        const { value, pattern } = e.target;
+        if(!pattern){
+            return
+        }
+        const isValid = new RegExp(pattern).test(value);
+        e.target.classList.toggle('input-correct', isValid);
+        e.target.classList.toggle('wrong-input-value', !isValid);
+        e.target['data-valid'] = isValid
+
+        console.log(e.target['data-valid'])
+    }
+
+    return (
+        <>
+            <div className="container">
+                <NotificationComponent position="top-right"/>
+                <form method="POST" id="contact-form" onSubmit={handleSubmit}>
+                    <p>Свяжитесь с нами</p>
+                    <input type="text"
+                           placeholder="Email"
+                           onChange={handleInputChange}
+                           ref={emailInput}
+                           required={true}
+                           pattern={regexPatterns.email.source}
+                           minLength={5}
+                           maxLength={50}
+                    />
+                    <textarea name="contact-form-message"
+                              cols="30"
+                              rows="10"
+                              ref={messageInput}
+                              required={true}
+                              placeholder="Ваше сообщение"
+                              minLength={20}
+                              maxLength={350}
+                    />
+                    <label>
+                        <ReCAPTCHA
+                            sitekey={process.env.REACT_APP_CAPTCHA_KEY}
+                            onChange={handleCaptchaChange}
+                        />
+                    </label>
+                    <input type="submit" value="Отправить"/>
+                </form>
+            </div>
+        </>
+    )
+}
---
+++ src/helpers/regex.js
@@ -0,0 +1,5 @@
+
+
+export const regexPatterns = {
+    email: new RegExp("^[a-zA-Z]+[\\w\\d]*@[a-zA-Z\\d]+\\.[a-zA-Z]+$"),
+};
