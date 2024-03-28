c52d5559ab261ea930a70b10d0f2f7e10f95524b
Sinkler521 <131878372+Sinkler521@users.noreply.github.com>
2024-03-28 23:39:05 +0200

    added functions for News component and styles

--- src/components/application/News/News.js
+++ src/components/application/News/News.js
@@ -1,21 +1,41 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import {Loader} from "../../Loader/Loader";
 import {Article} from "./Article/Article";
 import {useSelector} from "react-redux";
 
 import './News.css';
+import bgExample from "../../../assets/images/application/news/example.jpg";
 import axios from "axios";
 
 export const News = (props) => {
     const user = useSelector(state => state.user)
     const [newsLoaded, setNewsLoaded] = useState(false);
+    const [currentFirstArticle, setCurrentFirstArticle] = useState(null);
     const [currentNews, setCurrentNews] = useState(null);
+
+    const [addingArticle, setAddingArticle] = useState(false);
+
+    const navLinks = useRef([]);
 
     useEffect(() => {
         getLatestNews();
+        setNewsLoaded(true);
     }, []);
 
-    const getLatestNews = async () => {
+    const navLinkClick = (e) => {
+        navLinks.current.forEach(el => {
+            if(el){
+                el.classList.remove('news-active')
+            }
+        });
+        e.target.classList.add('news-active');
+    }
+
+    const getLatestNews = async (e) => {
+        if(e){
+            navLinkClick(e)
+        }
+        setAddingArticle(false);
         try{
             const response = await axios.get(
                 `${props.host.api}/getlatestnews/`,
@@ -26,13 +46,50 @@ export const News = (props) => {
                 });
 
             if(response.status === 200){
-                setCurrentNews(response.data)
-                setNewsLoaded(true);
+                const responseData = response.data;
+                if (Array.isArray(responseData) && responseData.length > 0) {
+                    setCurrentFirstArticle(responseData[0]);
+                    setCurrentNews(responseData.slice(1));
+                }
             }
         }
         catch (error) {
-            console.log('Error trying loading news', error)
+            console.log('Error trying loading news', error);
         }
+    }
+
+    const getAllNews = async (e) => {
+        if(e){
+            navLinkClick(e)
+        }
+        setAddingArticle(false);
+        try{
+            const response = await axios.get(
+                `${props.host.api}/getallnews/`,
+            {
+                    headers: {
+                        "Content-Type": "application/json",
+                    }
+                });
+
+             if(response.status === 200){
+                const responseData = response.data;
+                if (Array.isArray(responseData) && responseData.length > 0) {
+                    setCurrentFirstArticle(responseData[0]);
+                    setCurrentNews(responseData.slice(1));
+                }
+            }
+        }
+        catch (error) {
+            console.log('Error trying loading news', error);
+        }
+    }
+
+    const addArticle = (e) => {
+        if(e){
+            navLinkClick(e)
+        }
+        setAddingArticle(true);
     }
 
     return (
@@ -41,15 +98,37 @@ export const News = (props) => {
                 <div className="container news-container">
                     <div className="news-header">
                         <ul className="news-menu-all">
-                            <li className="news-active">Latest</li>
-                            <li>All</li>
+                            <li onClick={getLatestNews} className="news-active" ref={el => navLinks.current[0] = el}>Latest</li>
+                            <li onClick={getAllNews} ref={el => navLinks.current[1] = el}>All</li>
                         </ul>
-                        <ul className="news-menu-staff">
-                            <li>Add</li>
-                        </ul>
+                        {user.isStaff ?
+                            <ul className="news-menu-staff">
+                                <li onClick={addArticle} ref={el => navLinks.current[2] = el}>Add</li>
+                            </ul>
+                            :
+                            null
+                        }
                     </div>
                     <div className="news-content">
-
+                        {addingArticle ?
+                            null
+                            :
+                            <>
+                                <div className="news-first-article-container">
+                                    <a className="news-first-image-wrap" href="#">
+                                        <img src={bgExample} alt="No image"/>
+                                    </a>
+                                    <div><h1>Горожане коллективно вышли на субботник</h1></div>
+                                    <div><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium ad
+                                        aliquid
+                                        at corporis cumque deserunt dolores dolorum eos ex facere fugiat incidunt
+                                        laborum
+                                        minima modi mollitia nihil nobis, nostrum numquam, odio quasi quisquam rem sed
+                                        sequi! Explicabo hic omnis quo.</p></div>
+                                    <div><span>16/03/2024</span></div>
+                                </div>
+                            </>
+                        }
                     </div>
                 </div>
                 :
