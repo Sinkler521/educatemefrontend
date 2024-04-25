aff33e9ee49d73dccf14cb71ada2c8d40687bf1e
Sinkler521 <131878372+Sinkler521@users.noreply.github.com>
2024-04-25 03:02:19 +0300

    functions and jsx for creating new course, deletion of existing one

--- src/components/application/Products/productsComponents/ProductsAdmin/ProductsAdmin.js
+++ src/components/application/Products/productsComponents/ProductsAdmin/ProductsAdmin.js
@@ -1,11 +1,11 @@
 import {useSelector} from "react-redux";
 import {NavLink, useNavigate} from "react-router-dom";
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import './ProductsAdmin.css';
 import {Loader} from "../../../../Loader/Loader";
 import {toast} from "react-toastify";
 import axios from "axios";
-import {normalizeDate} from "../../../../../helpers/apiHelpers";
+import {fileToBase64, normalizeDate} from "../../../../../helpers/apiHelpers";
 
 export const ProductsAdmin = (props) => {
     const user = useSelector(state => state.user);
@@ -17,6 +17,15 @@ export const ProductsAdmin = (props) => {
 
     const [deleteCourseId, setDeleteCourseId] = useState(null);
     const [deleteCourseTitle, setDeleteCourseTitle] = useState(null);
+
+    const addCourseContainer = useRef();
+    const addCourseButtons = useRef();
+    
+    const newCourse = {
+        user_id: user.userId,
+        course: {},
+        stages: [],
+    }
 
     const toggleAddingNew = () => setAddingNew(!addingNew);
     const toggleAskingForDeletion = () => setAskingForDeletion(!askingForDeletion);
@@ -105,6 +114,112 @@ export const ProductsAdmin = (props) => {
         }
     }
 
+    const addNewCourse = async () => {
+
+        await addCourseDataFromForms();
+
+        try{
+            const response = await axios.post(
+                `${props.host.api}/adminaddcourse/`,
+                newCourse,
+                {
+                    headers: {
+                        "Content-Type": "application/json",
+                    }
+                });
+
+            if(response.status === 200 || response.status === 201){
+                console.log('Course has been successfully added');
+                toast.success('Course has been added!');
+                setAddingNew(false);
+                await getTeacherInfo();
+            }
+
+        } catch(error){
+            if(error.response){
+                if(error.response.status === 400){
+                    toast.warning('Some info missing');
+                    console.log(error, 'error');
+                }
+            } else{
+                toast.error('Something goes wrong');
+                console.log(error, 'error');
+            }
+        }
+    }
+
+    const addCourseDataFromForms = async () => {
+        const courseForms = document.querySelectorAll('.course-add-form');
+
+        // course part
+        const mainForm = courseForms[0];
+
+        const imageInput = mainForm.querySelector('input[type="file"]');
+        const imageFile = imageInput.files.length > 0 ? imageInput.files[0] : null;
+        const imageBase64 = imageFile ? await fileToBase64(imageFile) : '';
+
+        newCourse.course = {
+            title: mainForm.elements['title'].value || '',
+            description: mainForm.elements['description'].value || '',
+            image: imageBase64,
+            topic: mainForm.elements['topic'].value || '',
+            complexity: mainForm.elements['complexity'].value || 'Easy',
+        };
+
+        // stages part
+        for(let i = 1; i < courseForms.length; i++){
+            const imageFile = courseForms[i].files ? courseForms[i].files[0] : null
+            const imageBase64 = imageFile ? await fileToBase64(imageFile) : '';
+
+            const stage = {
+                title: courseForms[i].elements['title'].value || '',
+                description: courseForms[i].elements['description'].value || '',
+                image: imageBase64,
+                video: courseForms[i].elements['video'].value || '',
+                text: courseForms[i].elements['text'].value || '',
+                order: i - 1,
+            }
+
+            newCourse.stages.push(stage);
+        }
+        console.log(newCourse);
+    };
+
+    const createNewStage = () => {
+        const newForm = document.createElement('form');
+        newForm.classList.add('course-add-form');
+
+        const titleInput = document.createElement('input');
+        titleInput.placeholder = 'Title';
+        titleInput.required = true;
+
+        const descriptionInput = document.createElement('textarea');
+        descriptionInput.placeholder = 'Description';
+
+        const imageInput = document.createElement('input');
+        imageInput.type = 'file';
+
+        const videoInput = document.createElement('input');
+        videoInput.placeholder = 'Video (youtube link)';
+
+        const textInput = document.createElement('textarea');
+        textInput.placeholder = 'Course text';
+
+        titleInput.name = 'title';
+        descriptionInput.name = 'description';
+        imageInput.name = 'image';
+        videoInput.name = 'video';
+        textInput.name = 'text';
+
+        newForm.appendChild(titleInput);
+        newForm.appendChild(descriptionInput);
+        newForm.appendChild(imageInput);
+        newForm.appendChild(videoInput);
+        newForm.appendChild(textInput);
+
+        addCourseContainer.current.insertBefore(newForm, addCourseButtons.current);
+    };
+
     return (
         <>
         {teacherInfo ?
@@ -129,7 +244,33 @@ export const ProductsAdmin = (props) => {
                         </section>
                         <section className="productsadmin-section-main">
                             {addingNew ?
-                                null
+                                <>
+                                    <div className="productsadmin-adding-block">
+                                        <div className="productsadmin-back" onClick={toggleAddingNew}>
+                                            <i className="fa-solid fa-arrow-left"></i>
+                                            <div className="productsadmin-back-message">Back</div>
+                                        </div>
+                                        <div className="course-add-container" ref={addCourseContainer}>
+                                            <h2>New course</h2>
+                                            <form className="course-add-form">
+                                                <input name="title" type="text" placeholder="title" required={true}/>
+                                                <textarea name="description" placeholder="description" cols="30" rows="4"></textarea>
+                                                <input type="file" placeholder="image"/>
+                                                <input type="text" name="topic" placeholder="Topic"/>
+                                                <select name="complexity">
+                                                    <option value="easy">Easy</option>
+                                                    <option value="medium">Medium</option>
+                                                    <option value="hard">Hard</option>
+                                                </select>
+                                            </form>
+
+                                            <div className="course-add-button-group" ref={addCourseButtons}>
+                                                <button onClick={createNewStage}><i className="fa-solid fa-plus"></i></button>
+                                                <button onClick={addNewCourse}><i className="fa-solid fa-floppy-disk"></i></button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </>
                                 :
                                 <>
                                     <div className="productsadmin-courses">
